b805ebd98b76fc5b860d0d2c659b965dc9de393f
Sriniketh J <srinikethcr7@gmail.com>
2021-10-17 15:11:14 +0530

    toi added

---
+++ Coding Problems/C/TowerofHanoi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void TOH(int n, char x, char y, char z)
+{
+    if (n > 0)
+    {
+        TOH(n - 1, x, z, y);
+        printf("Move from %c to %c \n", x, y);
+        TOH(n - 1, z, y, x);
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter the Number of Disks: ");
+    scanf("%d", &n);
+    TOH(n, 'A', 'B', 'C');
+    return 0;
+}
